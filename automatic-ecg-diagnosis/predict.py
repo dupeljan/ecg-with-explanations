4bf877c51c01a2545572035a239f85069e9d9f26
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-03-14 20:53:37 +0300

    Eval 5th ptb fold on given model

--- automatic-ecg-diagnosis/predict.py
+++ automatic-ecg-diagnosis/predict.py
@@ -1,17 +1,24 @@
 import numpy as np
+import pandas as pd
 import warnings
 import argparse
 warnings.filterwarnings("ignore")
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
 from datasets import ECGSequence
+from sklearn.metrics import classification_report
+
+from generate_figures_and_tables import get_optimal_precision_recall
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get performance on test set from hdf5')
-    parser.add_argument('path_to_hdf5', type=str,
+    parser.add_argument('--path_to_hdf5', type=str,
                         help='path to hdf5 file containing tracings')
-    parser.add_argument('path_to_model',  # or model_date_order.hdf5
+    parser.add_argument('--path_to_labels_csv', type=str,
+                        help='path to labels csv file.', default='')
+    parser.add_argument('--path_to_model',  # or model_date_order.hdf5
                         help='file containing training model.')
     parser.add_argument('--dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
@@ -35,3 +42,15 @@ if __name__ == '__main__':
     np.save(args.output_file, y_score)
 
     print("Output predictions saved")
+
+    if args.path_to_labels_csv != '':
+        # Magic
+        columns=['1dAVb', 'RBBB', 'LBBB', 'SB', 'AF', 'ST']
+        threshold = np.array([0.31871256, 0.51558906, 0.5997846, 0.3313214, 0.50154346,
+                              0.4270824])
+        y_true = pd.read_csv(args.path_to_labels_csv).to_numpy()
+        # _, _, threshold = get_optimal_precision_recall(y_true, y_score)
+        mask = y_score > threshold
+        y_pred = np.zeros_like(y_score)
+        y_pred[mask] = 1
+        print(classification_report(y_true, y_pred, target_names=columns))
--- example_physionet.py
+++ example_physionet.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 import wfdb
+from wfdb import processing
 import ast
+import h5py
+import matplotlib.pyplot as plot
 
 def load_raw_data(df, sampling_rate, path):
     if sampling_rate == 100:
@@ -16,30 +19,48 @@ sampling_rate = 500
 
 # load and convert annotation data
 Y = pd.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
+Y = Y[Y.strat_fold == 5]
+
 Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 
 # Load raw signal data
 X = load_raw_data(Y, sampling_rate, path)
 
+X_resample = np.empty((X.shape[0], 4000, X.shape[2]))
+# Resample it to 400 Gz
+for n in range(X.shape[0]):
+    for ch in range(X.shape[2]):
+        X_resample[n, :, ch] = processing.resample_sig(X[n, :, ch], 500, 400)[0]
+
 # Load scp_statements.csv for diagnostic aggregation
 agg_df = pd.read_csv(path+'scp_statements.csv', index_col=0)
 agg_df = agg_df[agg_df.diagnostic == 1]
 
+map_ad_classes = [
+    '1AVB',
+    'CRBBB',
+    'CLBBB',
+    'SBRAD',
+    'AFIB',
+    'STACH',
+]
+
 def aggregate_diagnostic(y_dic):
-    tmp = []
+    one_hot = np.zeros((len(map_ad_classes)))
     for key in y_dic.keys():
-        if key in agg_df.index:
-            tmp.append(agg_df.loc[key].diagnostic_class)
-    return list(set(tmp))
+        if key in map_ad_classes:
+            one_hot[map_ad_classes.index(key)] = 1.
+    return one_hot
 
 # Apply diagnostic superclass
 Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
 
-# Split data into train and test
-test_fold = 10
-# Train
-X_train = X[np.where(Y.strat_fold != test_fold)]
-y_train = Y[(Y.strat_fold != test_fold)].diagnostic_superclass
-# Test
-X_test = X[np.where(Y.strat_fold == test_fold)]
-y_test = Y[Y.strat_fold == test_fold].diagnostic_superclass
+# Gen test dataset
+with h5py.File("ptb_5_fold.hdf5", 'w') as f:
+    f.create_dataset('ptb_5', data=X_resample)
+
+# Gen annotation
+with open('ptb_5_annotation.csv', 'w') as f:
+    data = np.stack(Y.diagnostic_superclass.array)
+    data = pd.DataFrame(data=data, columns=['1dAVb', 'RBBB', 'LBBB', 'SB', 'AF', 'ST']).to_csv(index=False)
+    f.write(data)
